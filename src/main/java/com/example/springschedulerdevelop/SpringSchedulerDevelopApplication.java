680b0f1f337f2ad152f18ceba834accd49b93cf2
dirtbagss <dirtbagag4@gmail.com>
2024-11-18 09:07:51 +0900

    Init

---
+++ src/main/java/com/example/springschedulerdevelop/SpringSchedulerDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springschedulerdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchedulerDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springschedulerdevelop/SpringSchedulerDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springschedulerdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulerDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
